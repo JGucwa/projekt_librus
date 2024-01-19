48291693380fd431838f2b024d0d4232118bd856
JGucwa <kubjaxxx@gmail.com>
2024-01-19 11:34:37 +0100

    Dodano Funkcjonalnosc Logowania

--- projekt_librus/projekt_librus/Views/Logowanie.xaml.cs
+++ projekt_librus/projekt_librus/Views/Logowanie.xaml.cs
@@ -16,5 +16,17 @@ namespace projekt_librus.Views
         {
             InitializeComponent();
         }
+        private async void LogowaniePrzycisk(object sender, EventArgs e)
+        {
+            var uzytkownicy = await App.Database.WszyscyUzytkownicyFilter(login.Text, haslo.Text);
+            if (login.Text.Length != 7 || uzytkownicy.Count == 0)
+            {
+                DisplayAlert("Blad", "Podano nieprawidlowe dane", "OK");
+                return;
+            }
+
+            var uzytkownik = uzytkownicy.ElementAt(0);
+            Navigation.PushAsync(new MainPage(uzytkownik));
+        }
     }
 }
