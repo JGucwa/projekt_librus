f7bd2c0313e30dea75c9c46335fd257871d56be6
JGucwa <kubjaxxx@gmail.com>
2024-01-19 10:42:37 +0100

    Zmieniono klase Przedmiot

--- projekt_librus/projekt_librus/Views/Ocena.cs
+++ projekt_librus/projekt_librus/Views/Ocena.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,12 @@ namespace projekt_librus.Views
 {
     public class Ocena
     {
+        [PrimaryKey, AutoIncrement]
         public int Ocena_Id { get; set; }    
         public int Przedmiot_Id { get; set; }    
         public float Stopien { get; set; }    
+        public DateTime Data {  get; set; }
+        public string Opis {  get; set; }
+        public int Okres {  get; set; }
     }
 }
