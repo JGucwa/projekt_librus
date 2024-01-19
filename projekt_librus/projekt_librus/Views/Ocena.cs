b3224b1a31aa23250ae6bd32ebe628b3f6a256e5
JGucwa <kubjaxxx@gmail.com>
2024-01-19 10:34:14 +0100

    Dodano klase Ocena

---
+++ projekt_librus/projekt_librus/Views/Ocena.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projekt_librus.Views
+{
+    public class Ocena
+    {
+        public int Ocena_Id { get; set; }    
+        public int Przedmiot_Id { get; set; }    
+        public float Stopien { get; set; }    
+    }
+}
