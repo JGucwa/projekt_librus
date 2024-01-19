3e293b16edc17dad8daa96f9ff4e5db079d3ebc4
JGucwa <kubjaxxx@gmail.com>
2024-01-19 10:36:45 +0100

    Dodano klase przedmiot

---
+++ projekt_librus/projekt_librus/Views/Przedmiot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projekt_librus.Views
+{
+    public class Przedmiot
+    {
+        public int Przedmiot_id { get; set; }
+        public string Nazwa { get; set; }
+    }
+}
